50d3aaa731f78b6bca1e5efeb6e0085c2ecf50f4
Amir Sidik <amirgengiz@gmail.com>
2022-07-05 15:16:53 +0530

    this is after connecting with mongo atlas using mongoose from nodejs. it will return a json array which is received from cloud mongo data collections

--- server.js
+++ server.js
@@ -1,31 +1,61 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 //for parsing the body in post request
 var bodyParser = require('body-parser');
 
-var users = [{
-    id: 1,
-    name: 'Amir Sidik',
-    age: 23,
-    email: 'mkamirsidik@gmail.com'
-}]
+const DB = 'mongodb+srv://amir272:makakamir@cluster0.ewelskd.mongodb.net/users?retryWrites=true&w=majority';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(()=>{
+    console.log("Connected")
+}).catch((err)=>{
+    console.log("Error in connection");
+});
+
+var UserSchema = mongoose.Schema({
+    id: Number,
+    name: String,
+    age: Number,
+    email: String
+});
+
+var User = mongoose.model('User', UserSchema, 'userlist');
+
+var users = [];
 
 app.get('/api/users', function(req, res) {
     return res.json(users);
 })
 
-app.get('/', function(req, res) {
-    return res.json(users);
+app.get('/', async function(req, res) {
+    const filter = {};
+    const all = await User.find(filter);
+    return res.json(all);
 })
 
-app.post('/api/users', function(req, res) {
+//this will get the post data from user and will return a json data
+
+app.post('/api/users', async function(req, res) {
     var user = req.body.user;
     users.push(user);
+    var user1 = new User(user);
+    //to insert one particular collection
+    user1.save((err, user2)=> {
+        if(err) return console.log(err);
+        console.log(user2.name + " saved to mongodb");
+    })
+    //trying to find all the users in the schema
+    const filter = {};
+    const all = await User.find(filter);
+    return res.json(all);
 
-    return res.json(users);
 })
 
 app.listen('3000', () => {
